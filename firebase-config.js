7bdea5e60d356504bc8cf79825b4aa140ba6093f
ahmed <muhammadahmed12590@gmail.com>
2025-10-31 03:12:55 +0500

    login page

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCEV4Nlpu7Hr-BxKyNHMl0r8SCYaVcQW1o",
+  authDomain: "ahmed-backend-7c222.firebaseapp.com",
+  projectId: "ahmed-backend-7c222",
+  storageBucket: "ahmed-backend-7c222.firebasestorage.app",
+  messagingSenderId: "716742749301",
+  appId: "1:716742749301:web:2e9557caa4902984c11c3c",
+  measurementId: "G-WCBF37XTH5"
+};
