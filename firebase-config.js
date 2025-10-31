eea499b59f719d08922c3a4fedb6051f850941fd
ahmed <muhammadahmed12590@gmail.com>
2025-11-01 00:09:36 +0500

    side bar minor changes

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // firebase-config.js
 
 export const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "ahmed-backend-7c222.firebaseapp.com",
-  projectId: "ahmed-backend-7c222",
-  storageBucket: "ahmed-backend-7c222.firebasestorage.app",
-  messagingSenderId: "716742749301",
-  appId: "1:716742749301:web:2e9557caa4902984c11c3c",
-  measurementId: "G-WCBF37XTH5"
+    apiKey: "your-api-key",
+    authDomain: "ahmed-backend-7c222.firebaseapp.com",
+    projectId: "ahmed-backend-7c222",
+    storageBucket: "ahmed-backend-7c222.firebasestorage.app",
+    messagingSenderId: "716742749301",
+    appId: "1:716742749301:web:2e9557caa4902984c11c3c",
+    measurementId: "G-WCBF37XTH5"
 };
